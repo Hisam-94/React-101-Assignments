e8636942526f5f6e0d3b2e2a2b611d05ad5addc6
Hisam-94 <hisamuddin94@gmail.com>
2022-05-23 19:31:12 +0530

    React-mobile-os

---
+++ react-mobile-os/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+     <h1>Mobile Operating System</h1>
+     <ul>
+       <li>Android</li>
+       <li>Blackberry</li>
+       <li>iPhone</li>
+       <li>Windows Phone</li>
+     </ul>
+     <h1>Mobile Manufacturers</h1>
+     <ul>
+       <li className="square">Samsung</li>
+       <li className="square">HTC</li>
+       <li>Micromax</li>
+       <li className="circle">Apple</li>
+     </ul>
+    </div>
+  );
+}
+
+export default App;
